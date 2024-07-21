7c4b628df93a5f0f7ea7b6f42cb2dce12ece593e
RamtinKosari <ramtinkosari@gmail.com>
2024-07-22 03:01:44 +0330

    [#REFACTOR] Add Method to Connect to the Database

---
+++ Configs.py
@@ -0,0 +1,43 @@
+# - Import Necessary Libraries
+import psycopg2
+import os
+
+# - Database Terminal Output
+DATABASE = f'\033[38;2;0;173;173m[RKDBH]\033[0m'
+# - Warning Terminal Output
+WARNING = f'\033[38;2;255;255;0m[WARNING]\033[0m'
+# - Success Terminal Output
+SUCCESS = f'\033[38;2;0;232;0m[SUCCESS]\033[0m'
+# - Failed Terminal Output
+FAILED = f'\033[38;2;255;0;0m[FAILED]\033[0m'
+# - Log Terminal Output
+LOG = f'\033[38;2;153;153;153m[LOG]\033[0m'
+# - Info Color
+INFO = f'\033[38;2;106;140;150m'
+# - Error Color
+ERR = f'\033[38;2;255;200;0m'
+# - Reset Color
+RESET = f'\033[0m'
+
+# - Yellow Color
+YELLOW = f'\033[38;2;255;255;0m'
+# - Cyan Color
+CYAN = f'\033[38;2;0;200;200m'
+# - Green Color
+GREEN = f'\033[38;2;0;170;0m'
+# - Red Color
+RED = f'\033[38;2;255;0;0m'
+
+# symbols = "✓⚠❖⁂✘✗ ☑☐☒༻◆◈☓⬢⬡"
+
+# - Log Messages
+LOG_MESSAGES = True
+
+# - Log Failures
+LOG_FAILURES = True
+
+# - Connection Auto Commit
+CONNECTION_AUTOCOMMIT = True
+
+# - Disable Cursor Blinking
+DISABLE_CURSOR_BLINKING = False
---
+++ DBH.py
@@ -0,0 +1,78 @@
+# - Import Configurations
+from .Configs import *
+from .Queries import *
+
+##
+# Holds Methods for Database Handling 
+##
+class DBHandler():
+    # - Method to Initialize DBHandler Object 
+    def __init__(
+        self,
+        _database: str = None,
+        _password: str = None,
+        _user: str = None,
+        _host: str = None
+    ):
+        # - Database Cursor Object
+        self.cursor = None
+        # - Database Connection Object
+        self.connection = None
+        # - Connection Status
+        self.connectionStatus = False
+        # - Database Connection Parameters
+        self.DBParameters = {
+            "password": _password,
+            "database": _database,
+            "host": _host,
+            "user": _user
+        }
+    # - Method to Connect to Database
+    def connect(
+        self,
+        _auto_commit: bool = CONNECTION_AUTOCOMMIT,
+        _database: str = None,
+        _password: str = None,
+        _user: str = None,
+        _host: str = None
+    ):
+        try:
+            # - Check id DBParameters are Set
+            if self.DBParameters["database"] is None:
+                # - Check if DB Parameters are Provided as Parameters
+                if _database != None and _password != None and _user != None and _host != None:
+                    # - Set DB Parameters
+                    self.DBParameters = {
+                        "password": _password,
+                        "database": _database,
+                        "host": _host,
+                        "user": _user
+                    }
+                else:
+                    if LOG_FAILURES:
+                        print(DATABASE, FAILED, "Database Parameters are Missing")
+                    return False
+            # - Connect to Database
+            self.connection = psycopg2.connect(**self.DBParameters)
+            # - Set Auto Commit
+            self.connection.autocommit = _auto_commit
+            # - Create Cursor
+            self.cursor = self.connection.cursor()
+            # - Set Connection Status
+            self.connectionStatus = True
+            # - Print Connection Status
+            if LOG_MESSAGES:
+                print(DATABASE, SUCCESS, "Connected to the Database {}{}{}".format(INFO, self.DBParameters["database"], RESET))
+            # - Disable Cursor Blinking
+            if DISABLE_CURSOR_BLINKING:
+                os.system("tput civis")
+            # - Return True
+            return True
+        except Exception as e:
+            if LOG_FAILURES:
+                print(DATABASE, FAILED, "Can Not Connect to the Database {}{}{}".format(ERR, e, RESET))
+            # - Set Connection Status
+            self.connectionStatus = False
+            # - Return False
+            return False
+    # - Method to Disconnect from Database
